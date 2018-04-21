fae33fe5dc7d1fef17b53015ae8d10644f09d053
Guozhen Li <ligz@ucdavis.edu>
2018-04-20 18:25:01 -0700

    Make Local_module output zip file of ArcGIS location-allocation results

--- Local_module/python/Minimize_HRS_for_Local_H2Demands.py
+++ Local_module/python/Minimize_HRS_for_Local_H2Demands.py
@@ -1,19 +1,19 @@
 import os
 import sys
+import zipfile
 import arcpy
 from datetime import datetime
 import pandas as pd
 
-
+SHAPEFILE_EXT = ['.shp', '.shx', '.dbf', '.prj', '.xml', '.sbn', '.sbx', '.cpg']
 
 try:
     print datetime.now().time(), "- program start"
-
-
+    
     # Set working env
     ## Paths
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    module_dir = os.path.join(script_dir, '..')
+    module_dir = os.path.normpath(os.path.join(script_dir, '..'))
     with open(os.path.join(module_dir, 'active_scenario.txt')) as f:
         scenario = f.readline().strip()
     print 'Active scenario:', scenario
@@ -89,14 +89,11 @@ try:
         network_dataset_path = os.path.join(module_dir, "TIGER_ND.gdb", "ITN_TIGER_2017", "ITN_TIGER_2017_ND")
 
         for coverageMinutes in coverageMinutesRange:
-            # Set Location Allocation output layer
-            out_NA_layer_name = "Local_Min_Facilities_{}_{:02d}min_coverage".format(cy, coverageMinutes)
-            # out_NA_layer_path = os.path.join(gdb_path, out_NA_layer_name)
-
-            # Create Location-Allocation layer
-            out_NA_layer = arcpy.MakeLocationAllocationLayer_na(
+            # Create Location Allocation layer
+            NA_layer_name = "Local_Min_Facilities_{}_{:02d}min_coverage".format(cy, coverageMinutes)
+            NA_layer = arcpy.MakeLocationAllocationLayer_na(
                 in_network_dataset = network_dataset_path,
-                out_network_analysis_layer = out_NA_layer_name,
+                out_network_analysis_layer = NA_layer_name,
                 impedance_attribute = "Minutes",
                 loc_alloc_from_to = "DEMAND_TO_FACILITY",
                 loc_alloc_problem_type = "MINIMIZE_FACILITIES",
@@ -104,16 +101,16 @@ try:
             ).getOutput(0)
 
             #Get the names of all the sublayers within the location-allocation layer.
-            subLayerNames = arcpy.na.GetNAClassNames(out_NA_layer)
-            print "\tCreated a Location-Allocation Layer named {0}. Sub-layers: {1}.".format(out_NA_layer_name, subLayerNames)
+            subLayerNames = arcpy.na.GetNAClassNames(NA_layer)
+            print "\tCreated a Location-Allocation Layer named {0}. Sub-layers: {1}.".format(NA_layer_name, subLayerNames)
             print datetime.now().time(), "- completed creating Location-Allocation layer."
 
-
-            # Load the candidate station locations as facilities using default search tolerance and field mappings.
-            candidateFieldMappings = arcpy.na.NAClassFieldMappings(out_NA_layer, subLayerNames["Facilities"])
+            # Load the candidate station locations as "Facilities" 
+            # using default search tolerance and field mappings.
+            candidateFieldMappings = arcpy.na.NAClassFieldMappings(NA_layer, subLayerNames["Facilities"])
             candidateFieldMappings["Name"].mappedFieldName = "hub.name"
             arcpy.na.AddLocations(
-                in_network_analysis_layer = out_NA_layer,
+                in_network_analysis_layer = NA_layer,
                 sub_layer = subLayerNames["Facilities"],
                 in_table = candidate_sites_layer,
                 field_mappings = candidateFieldMappings,
@@ -121,38 +118,44 @@ try:
                 exclude_restricted_elements = "EXCLUDE")
 
             # Load the demand points using default search tolerance
-            # Use the field mappings to map the Weight property from Daily.kgH2 field.
-            demandFieldMappings = arcpy.na.NAClassFieldMappings(out_NA_layer,
+            # Use Daily.kgH2 field to map to the Weight property.
+            demandFieldMappings = arcpy.na.NAClassFieldMappings(NA_layer,
                                                                 subLayerNames["DemandPoints"])
             demandFieldMappings["Weight"].mappedFieldName = "Daily.kgH2"
             demandFieldMappings["Name"].mappedFieldName = "Hub"
             arcpy.na.AddLocations(
-                in_network_analysis_layer = out_NA_layer,
+                in_network_analysis_layer = NA_layer,
                 sub_layer = subLayerNames["DemandPoints"],
                 in_table = demand_points_layer,
                 field_mappings = demandFieldMappings,
                 search_tolerance = "",
                 exclude_restricted_elements = "EXCLUDE")
-            print "\tAdded locations to the Location-Allocatin Layer."
+            print "\tAdded facility and demand locations to the Location-Allocatin Layer."
             print datetime.now().time(), "- complete adding locations to Location-Allocation layer."
 
 
-            #Solve the location-allocation layer
+            # Solve the location-allocation layer
             print "\tNetwork Analyst is solving..."
-            arcpy.na.Solve(out_NA_layer)
-            print datetime.now().time(), "- complete solving."
+            arcpy.Solve_na(NA_layer)
+            print datetime.now().time(), "- completed solving."
 
-            #Save the solved location-allocation layer as a layer file on disk with relative paths
-            # arcpy.management.SaveToLayerFile(out_NA_layer, , "RELATIVE")
-            # out_NA_layer.saveACopy(os.path.join(scenario_dir, out_NA_layer_name+".lyrx"))
-
-            facilitiesLayer = arcpy.mapping.ListLayers(out_NA_layer, "Facilities")[0]
-            desc = arcpy.Describe(facilitiesLayer)
+            # Save the solved "Facilities" layer, as shapefile
+            facilitiesLayer = arcpy.mapping.ListLayers(NA_layer, "Facilities")[0]
+            out_shapefile_path = os.path.join(out_shapefile_dir, NA_layer_name)
+            print 'Saving solved Facilities layer to {}.'.format(out_shapefile_path) 
+            print 'Description:', arcpy.Describe(facilitiesLayer)
             arcpy.MakeFeatureLayer_management(in_features = facilitiesLayer,
                                               out_layer = "chosen_facilities_lyr",
-                                              where_clause = "FacilityType=3")
-            out_shp_filename = os.path.join(out_shapefile_dir, out_NA_layer_name)
-            arcpy.CopyFeatures_management("chosen_facilities_lyr",  out_shp_filename)
+                                              where_clause = "FacilityType=3")  # FacilityType=3 means selected facilities
+            arcpy.CopyFeatures_management("chosen_facilities_lyr",  out_shapefile_path)
+
+            # Zip the shapefile into a zip file
+            os.chdir(out_shapefile_dir)
+            files_to_zip = [f for f in os.listdir(out_shapefile_dir) if f.startswith(NA_layer_name) and os.path.splitext(f)[1] in SHAPEFILE_EXT]
+            with zipfile.ZipFile(file=os.path.join(NA_layer_name+'.zip'), mode='w') as z:
+                for f in files_to_zip:
+                    z.write(f)
+
 
 
 except Exception as e:
